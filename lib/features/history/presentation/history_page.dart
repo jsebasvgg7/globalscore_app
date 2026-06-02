99aca6a1f1c9c20444e51f3bbc9dbd9856e023f3
jsebasvgg7 <johnsebastianveggonzalez@gmail.com>
2026-06-01 22:02:18 -0500

    v1.13

--- lib/features/history/presentation/history_page.dart
+++ lib/features/history/presentation/history_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../domain/history_providers.dart';
 import 'pages/history_vault_page.dart';
-import 'pages/history_competitions_page.dart';
-import '../presentation/pages/history_events_page.dart';
+import 'pageCompes/history_competitions_page.dart';
+import 'pageEvents/history_events_page.dart';
 import 'pageTeams/history_teams_page.dart';
 import 'pagePlayers/history_players_page.dart';
 
